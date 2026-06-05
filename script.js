b1aa8f4b584cda57be591b2e2d3a2c3640ab9cd1
JuuKabyar <177juu17@gmail.com>
2026-06-05 18:43:09 +0630

    Edit for responsive

--- script.js
+++ script.js
@@ -74,3 +74,16 @@ function openProject(projectName) {
 function closeProject() {
   document.getElementById("projectModal").style.display = "none";
 }
+
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
